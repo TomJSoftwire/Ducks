9450a6edac10f76483a14b17ea37a636913ffcda
TomJSoftwire <thomas.johnston@softwire.com>
2020-11-18 14:55:43 +0000

    Initial commit

---
+++ Test/com/company/Test.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Test {
+    public static void main(String[] args) {
+        Duck model = new ModelDuck();
+        model.performFly();
+        model.performQuack();
+        model.setFlyBehavior(new FlyRocketPowered());
+        model.performFly();
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import javax.sound.midi.Soundbank;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+}
